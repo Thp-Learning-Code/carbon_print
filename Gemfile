b23c3b23addc0c433412974bbfa1e392b0948836
slim2244 <seb_93@live.fr>
2019-03-12 23:33:40 +0100

    Map done, need to change the icon

--- Gemfile
+++ Gemfile
@@ -44,6 +44,8 @@ gem 'devise', '~> 4.6'
 gem 'dotenv-rails'
 gem 'rb-readline', '~> 0.5.5'
 gem 'nokogiri'
+gem 'geocoder'
+gem 'mapbox-gl-rails'
 
 group :development, :test do
   # Call 'byebug' anywhere in the code to stop execution and get a debugger console
--- db/seeds.rb
+++ db/seeds.rb
@@ -48,11 +48,11 @@ puts "Brand created"
 
 
 5.times do |i|
-  Product.create!(title: Faker::Name.first_name, description: Faker::WorldCup.team, price: rand(1..30), type_id: Type.all.sample.id, brand_id: Brand.all.sample.id)
+  Product.create!(title: Faker::Name.first_name, description: Faker::WorldCup.team, price: rand(1..30),city: "Londres", country: "Angleterre", type_id: Type.all.sample.id, brand_id: Brand.all.sample.id)
 end
 puts "Products created"
 
 5.times do |i|
-  FootPrint.create!(delivery_address: Faker::Address.street_address, zip_code: Faker::Address.zip_code, user_id: User.all.sample.id, product_id: Product.all.sample.id )
+  FootPrint.create!(delivery_address: Faker::Address.street_address, zip_code: Faker::Address.zip_code, town: "Paris", country: "France", user_id: User.all.sample.id, product_id: Product.all.sample.id)
 end
 puts "Footprint created"
