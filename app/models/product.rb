2525cd8faf00964e1946cd7a756c642ef3d792ab
slim2244 <seb_93@live.fr>
2019-03-13 14:37:08 +0100

    Add map foot_print_show

--- app/models/product.rb
+++ app/models/product.rb
@@ -3,4 +3,10 @@ class Product < ApplicationRecord
   has_many :users, through: :footprints
   belongs_to :type
   belongs_to :brand
+  geocoded_by :location
+  after_validation :geocode
+
+  def location
+    [city, country].compact.join(", ")
+  end
 end
