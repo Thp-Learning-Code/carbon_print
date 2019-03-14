8e153a1dc211a43920adb4dff1b93f6f945084a7
jiaad <abdulziad08@outlook.com>
2019-03-14 15:45:54 +0100

    added routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,10 @@
 Rails.application.routes.draw do
+  get 'product/index'
+  get 'product/show'
   devise_for :users
   resources :foot_prints
   root to: 'home#index'
+  resources :product
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
