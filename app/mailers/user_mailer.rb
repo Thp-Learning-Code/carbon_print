a73aff5015b89001cf694945ee0097c25bad76e8
TheMindset <mulezig2014@outlook.fr>
2019-03-14 19:33:05 +0100

    mailer in production ==> ok

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -5,9 +5,14 @@ class UserMailer < ApplicationMailer
     @user = user
 
     #on définit une variable @url qu'on utilisera dans la view d’e-mail
-    @url  = 'http://monsite.fr/login'
+    @url  = 'corsairethp@gmail.com'
 
     # c'est cet appel à mail() qui permet d'envoyer l’e-mail en définissant destinataire et sujet.
-    mail(to: @user.email, subject: 'Welcome to My Site', delivary_method_options: { version: 'v3.1', api_key: ENV['MAIL_JET_API_PUBLIQUE'], secret_key: ENV['MAIL_JET_API_SECRET'] })
+    mail(to: @user.email,
+      subject: 'Welcome to My Site',
+      delivary_method_options: { version: 'v3.1',
+      api_key: ENV['MAIL_JET_API_PUBLIQUE'],
+      secret_key: ENV['MAIL_JET_API_SECRET']
+      })
   end
 end
