3c0b29553965f3c757f0e32476362727181c53af
bamss <amadougolfa98@gmail.com>
2019-03-15 17:27:41 +0100

    edit user model

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,13 +1,15 @@
 class User < ApplicationRecord
 
-  validates :last_name, presence: true
-  validates :first_name, presence: true
+  
 
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 
+         validates :last_name, presence: true
+         validates :first_name, presence: true
+         
   has_many :products, through: :footprints
   
 end
