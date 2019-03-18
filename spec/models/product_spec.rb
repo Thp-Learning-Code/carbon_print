5a4d2669bf2b3bc6b229cfc086c8d89dd43a646f
bamss <amadougolfa98@gmail.com>
2019-03-18 10:47:57 +0100

    test product

--- spec/models/product_spec.rb
+++ spec/models/product_spec.rb
@@ -1,12 +1,13 @@
 require 'rails_helper'
 
 RSpec.describe Product, type: :model do
+
  it "should create a product" do 
-    Value.create(carbon_print: 21)
-    Type.create(name: "jeans", value_id: 2)
-    Ratio.create(carbon_print_for_brand: 18)
-    Brand.create(name: 'Apple', ratio_id: 1)
-    Product.create(title: "Iphone", brand_id: 1, type_id: 1, price: 18, description: "smartphone Apple")
+    value = Value.create(carbon_print: 21)
+    type = Type.create(name: "jeans", value_id: value.id)
+    ratio = Ratio.create(carbon_print_for_brand: 18)
+    brand = Brand.create(name: 'Apple', ratio_id: ratio.id)
+    Product.create(title: "Iphone", brand_id: brand.id, type_id: type.id, price: 18, description: "smartphone Apple")
     expect(Product.count).to eq(1)
  end
 end
