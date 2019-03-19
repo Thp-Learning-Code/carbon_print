eec17372a8d838ed4330e46004a790be2571ac6c
jiaad <abdulziad08@outlook.com>
2019-03-19 23:14:33 +0100

    added town city country  in devise logic

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -7,8 +7,8 @@ class ApplicationController < ActionController::Base
  before_action :configure_permitted_parameters, if: :devise_controller?
 	 protected
 	def configure_permitted_parameters
-    devise_parameter_sanitizer.permit(:sign_up, keys: [:first_name, :last_name,:email,:password])
-    devise_parameter_sanitizer.permit(:account_update, keys: [:first_name, :last_name,:email,:password])
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:first_name, :last_name,:email,:password,:town,:zip_code,:country])
+    devise_parameter_sanitizer.permit(:account_update, keys: [:first_name, :last_name,:email,:password,:town,:zip_code,:country])
   end
 
   def authorize_admin
