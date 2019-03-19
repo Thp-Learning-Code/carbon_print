dfada32cb0bd5e47d7b54de97f68239772a84799
slim2244 <seb_93@live.fr>
2019-03-19 21:47:49 +0100

    add database Type

--- db/seeds.rb
+++ db/seeds.rb
@@ -9,51 +9,25 @@
 
 require 'faker'
 
-# User.delete_all
-# Product.delete_all
+User.delete_all
+Product.delete_all
 # Footprint.delete_all
-# Value.delete_all
-# Type.delete_all
+Value.delete_all
+Type.delete_all
 Ratio.delete_all
 Brand.delete_all
-# ActiveRecord::Base.connection.reset_pk_sequence!('users')
-# ActiveRecord::Base.connection.reset_pk_sequence!('products')
+ActiveRecord::Base.connection.reset_pk_sequence!('users')
+ActiveRecord::Base.connection.reset_pk_sequence!('products')
 # ActiveRecord::Base.connection.reset_pk_sequence!('footprints')
-# ActiveRecord::Base.connection.reset_pk_sequence!('values')
-# ActiveRecord::Base.connection.reset_pk_sequence!('types')
+ActiveRecord::Base.connection.reset_pk_sequence!('values')
+ActiveRecord::Base.connection.reset_pk_sequence!('types')
 ActiveRecord::Base.connection.reset_pk_sequence!('brands')
 ActiveRecord::Base.connection.reset_pk_sequence!('ratios')
 
-# 5.times do |i|
-#  User.create!(first_name: Faker::Name.first_name, last_name: Faker::Name.last_name, email: Faker::Internet.email, password: Faker::Bank.account_number)
-# end
-# puts "Users created"
-
-# 5.times do |i|
-#   Value.create!(carbon_print: rand(1..30))
-# end
-# puts "Value created"
-
-# 5.times do |i|
-#   Type.create!(name: "hola", value_id: Value.all.sample.id )
-# end
-# puts "Type created"
-
-# 5.times do |i|
-#   Ratio.create!(carbon_print_for_brand: rand(1..30))
-# end
-# puts "Ratio created"
-
-# 5.times do |i|
-#   Brand.create!(name: Faker::WorldCup.group, ratio_id: Value.all.sample.id )
-# end
-# puts "Brand created"
-
-
-# 5.times do |i|
-#   Product.create!(title: Faker::Name.first_name, description: Faker::WorldCup.team, price: rand(1..30),city: "Londres", country: "Angleterre", type_id: Type.all.sample.id, brand_id: Brand.all.sample.id)
-# end
-# puts "Products created"
+5.times do |i|
+ User.create!(first_name: Faker::Name.first_name, last_name: Faker::Name.last_name, email: Faker::Internet.email, password: Faker::Bank.account_number)
+end
+puts "Users created"
 
 # 5.times do |i|
 #   Footprint.create!(delivery_address: Faker::Address.street_address, zip_code: Faker::Address.zip_code, town: "Paris", country: "France", user_id: User.all.sample.id, product_id: Product.all.sample.id)
@@ -84,14 +58,31 @@ end
 
 puts "There are now #{Value.count} rows in the Value table"
 
-# csv_text = File.read(Rails.root.join('lib', 'seeds', 'Brand.csv'))
-# csv = CSV.parse(csv_text, :headers => true, :encoding => 'ISO-8859-1')
-# csv.each do |row|
-#   t = Brand.new
-#   t.name = row['name']
-#   t.ratio_id = row['ratio_id']
-#   t.save
-#   puts "Brand saved"
-# end
+csv_text = File.read(Rails.root.join('lib', 'seeds', 'Brand.csv'))
+csv = CSV.parse(csv_text, :headers => true, :encoding => 'ISO-8859-1', :col_sep => ';')
+csv.each do |row|
+  t = Brand.new
+  t.name = row[0]
+  t.ratio_id = row[1]
+  t.save
+  puts "Brand saved"
+end
 
-# puts "There are now #{Brand.count} rows in the Brand table"
+puts "There are now #{Brand.count} rows in the Brand table"
+
+csv_text = File.read(Rails.root.join('lib', 'seeds', 'Type.csv'))
+csv = CSV.parse(csv_text, :headers => true, :encoding => 'ISO-8859-1', :col_sep => ';')
+csv.each do |row|
+  t = Type.new
+  t.name = row[0]
+  t.value_id = row[1]
+  t.save
+  puts "Type saved"
+end
+
+puts "There are now #{Type.count} rows in the Type table"
+
+5.times do |i|
+  Product.create!(title: Faker::Name.first_name, description: Faker::WorldCup.team, price: rand(1..30),city: "Londres", country: "Angleterre", type_id: Type.all.sample.id, brand_id: Brand.all.sample.id)
+end
+puts "Products created"
