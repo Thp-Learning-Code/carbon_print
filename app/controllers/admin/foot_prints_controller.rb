a02453e81b2e73dc0446d8aa1ff5aeab4394fbec
jiaad <abdulziad08@outlook.com>
2019-03-19 16:36:42 +0100

    want to merge

--- app/controllers/admin/foot_prints_controller.rb
+++ app/controllers/admin/foot_prints_controller.rb
@@ -1,15 +1,15 @@
 class Admin::FootPrintsController < ApplicationController
   def index
-    @foot_print = FootPrint.all
+    @foot_print = Footprint.all
   end
 
   def show
-    @foot_print = FootPrint.find(params[:id])
+    @foot_print = Footprint.find(params[:id])
   end
 
 
   def destroy
-    @foot_print = FootPrint.find(params[:id])
+    @foot_print = Footprint.find(params[:id])
     if @foot_print.destroy
         flash[:success]="Test deleted"
         redirect_back(fallback_location: admin_foot_print_path)
