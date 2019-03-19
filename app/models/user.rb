bbd930a4bef0149a49a0779e15ebf779c9e7da89
jiaad <abdulziad08@outlook.com>
2019-03-19 23:13:27 +0100

    added validates for user.rb country, zipcode,town

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,6 +2,10 @@ class User < ApplicationRecord
 
   validates :last_name, presence: true
   validates :first_name, presence: true
+  validates :town, presence: true
+  validates :country, presence: true
+  validates :first_name, presence: true
+
 
 
   # Include default devise modules. Others available are:
