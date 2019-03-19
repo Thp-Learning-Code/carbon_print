d430a90b79ab1e1729a3d526c9cd7a7a8b13553b
jiaad <abdulziad08@outlook.com>
2019-03-19 17:59:46 +0100

    conflict fixed

--- app/controllers/user_controller.rb
+++ app/controllers/user_controller.rb
@@ -1,10 +1,6 @@
 class UserController < ApplicationController
   def show
     @user = User.find(params[:id])
-<<<<<<< HEAD
-    @foot_print = Footprint.where(:user_id =>@user.id)
-=======
     @foot_print = Footprint.where(:user_id => params[:id])
->>>>>>> 48cad2beb7e3d9b528e81952e47540f95f1e028a
   end
 end
