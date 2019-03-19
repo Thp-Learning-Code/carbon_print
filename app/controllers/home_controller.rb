9bf5fedb4cc695b9e2f7b1cc21dbaa3dfd43bb3b
jiaad <abdulziad08@outlook.com>
2019-03-19 11:49:41 +0100

    added logic for user who hasn't gave any town: so there will not be any error after signing in: to see air_visual api

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -6,13 +6,13 @@ class HomeController < ApplicationController
     page = Nokogiri::HTML(open("https://maconscienceecolo.com/knowledge-base/100-gestes-ecolos/"))
     n = rand(0..99)
     @tips = page.xpath('//*[@id="kb-article-content"]/ol/li')[n].text
-    if user_signed_in?
+    if user_signed_in? && current_user.town != nil
       url = "https://api.airvisual.com/v2/nearest_city?lat=#{current_user.latitude}&lon=#{current_user.longitude}&key=BbFES4LurEehNo6PR"
       url_two ='https://api.airvisual.com/v2/city?city=Paris&state=Ile-de-France&country=France&key=BbFES4LurEehNo6PR'
       puts @response = open(url).read 
       puts current_user.latitude
-      # puts "*"*90
-      # puts @response.to_json
+      puts "*"*90
+      puts current_user.town
       @res = JSON.parse(@response).with_indifferent_access
         @aqius = @res[:data][:current][:pollution][:aqius]
         @res[:data][:current][:pollution][:mainus]
