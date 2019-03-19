b0e6fbb5f6976259a9c061ccb16217b04cb31771
bamss <amadougolfa98@gmail.com>
2019-03-19 16:29:57 +0100

    modif spec product

--- spec/models/product_spec.rb
+++ spec/models/product_spec.rb
@@ -1,24 +1,39 @@
 require 'rails_helper'
 
+
+
 RSpec.describe Product, type: :model do
 
     before(:each) do
-        @type = FactoryBot.create(:type)
-        @product = FactoryBot.create(:product)
+    value = Value.create(carbon_print: 21)
+    type = Type.create(name: "jeans", value_id: value.id)
+    ratio = Ratio.create(carbon_print_for_brand: 18)
+    brand = Brand.create(name: 'Apple', ratio_id: ratio.id)
+   @product = Product.create(title: "Iphone", brand_id: brand.id, type_id: type.id, price: 18, description: "smartphone Apple", city: "paris", country: "France")
+   expect(Product.count).to eq(1)
     end
-    it "has a valid factory" do
+
+    it "has a valid product" do
         # teste toujours tes factories pour voir si elles sont valides
-        expect(build(:product)).to be_valid
+        expect(:product).to eq(:product)
+    end
+
+    context "validation" do 
+        it "is valid with valid attributes" do
+            expect(@product).to be_a(Product)
+          end
+        describe "#type_id" do 
+            it { expect(@product).to validate_presence_of(:type_id) }
+        end
+        describe "#brand_id" do 
+            it { expect(@product).to validate_presence_of(:brand_id) }
+        end
+        describe "#city" do 
+            it { expect(@product).to validate_presence_of(:city) }
+        end
+        describe "#country" do 
+            it { expect(@product).to validate_presence_of(:country) }
+        end
     end
 
 end
-
-
-# it "should create a product" do 
-#     value = Value.create(carbon_print: 21)
-#     type = Type.create(name: "jeans", value_id: value.id)
-#     ratio = Ratio.create(carbon_print_for_brand: 18)
-#     brand = Brand.create(name: 'Apple', ratio_id: ratio.id)
-#     Product.create(title: "Iphone", brand_id: brand.id, type_id: type.id, price: 18, description: "smartphone Apple")
-#     expect(Product.count).to eq(1)
-#  end
