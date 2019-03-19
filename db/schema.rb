a0b9786a56d4059048f6a3a36bb65a163cc35847
jiaad <abdulziad08@outlook.com>
2019-03-19 14:10:06 +0100

    added migration

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_03_18_152613) do
+ActiveRecord::Schema.define(version: 2019_03_19_123409) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -85,6 +85,7 @@ ActiveRecord::Schema.define(version: 2019_03_18_152613) do
     t.float "latitude"
     t.float "longitude"
     t.string "country"
+    t.boolean "is_admin", default: false
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
