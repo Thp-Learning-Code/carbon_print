ddfad2d2592c21056f62f41f5af741cae9efba5f
bamss <amadougolfa98@gmail.com>
2019-03-20 11:01:56 +0100

    modif models footprint

--- spec/models/foot_print_spec.rb
+++ spec/models/foot_print_spec.rb
@@ -1,14 +1,32 @@
 require 'rails_helper'
 
 RSpec.describe FootPrint, type: :model do
-    it "should create a product foot_print"  do
+
+
+  before(:each) do
       user =  User.create(first_name: 'bob', last_name: 'lenon', zip_code: 75000, town: 'Butte chaumont', email: "test@gmail.com", password: 123456)
       value = Value.create(carbon_print: 21)
       type = Type.create(name: "jeans", value_id: value.id)
       ratio = Ratio.create(carbon_print_for_brand: 18)
       brand = Brand.create(name: 'Apple', ratio_id: ratio.id)
       product = Product.create(title: "Iphone", brand_id: brand.id, type_id: type.id, price: 18, description: "smartphone Apple")
-      FootPrint.create(delivery_address: "17 avenue simon bolivar", zip_code: 75019, user_id: user.id, product_id: product.id)
-      expect(FootPrint.count).to eq(1)
+      @FootPrint = FootPrint.create(delivery_address: "17 avenue simon bolivar", zip_code: 75019, user_id: user.id, product_id: product.id)
+      expect(@FootPrint).to be_a(FootPrint)
     end
+
+    context "validation" do
+      it "is valid with valid attributes" do
+        expect(@FootPrint).to be_a(FootPrint)
+      end
+      describe "#zip_code" do
+        it { expect(@FootPrint).to validate_presence_of(:zip_code) }
+      end
+      describe "#user_id" do
+        it { expect(@FootPrint).to validate_presence_of(:user_id) }
+      end
+      describe "#product_id" do
+        it { expect(@FootPrint).to validate_presence_of(:product_id) }
+      end
+    end
+
 end
