a0725c64160700a337b57f215ae88ec864b174ce
TheMindset <mulezig2014@outlook.fr>
2019-03-20 07:14:45 +0100

    connexion with facebook work !!!

--- app/models/user.rb
+++ app/models/user.rb
@@ -23,7 +23,8 @@ class User < ApplicationRecord
     where(auth.slice(:provider, :uid)).first_or_initialize.tap do |user|
       user.provider = auth.provider
       user.uid = auth.uid
-      user.email = Devise.email
+      user.email = auth.info.email
+      user.password = Devise.friendly_token[0, 20]
       user.name = auth.info.name
       user.oauth_token = auth.credentials.token
       user.oauth_expires_at = Time.at(auth.credentials.expires_at)
