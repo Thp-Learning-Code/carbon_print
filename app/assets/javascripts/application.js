64021923dd3106d84475e9d9c50b3dff83ad02fa
jiaad <abdulziad08@outlook.com>
2019-03-20 17:36:43 +0100

    conlict

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -10,18 +10,13 @@
 // Read Sprockets README (https://github.com/rails/sprockets#sprockets-directives) for details
 // about supported directives.
 //
-
 //= require rails-ujs
 //= require activestorage
 //= require turbolinks
-
 //= require jquery3
 //= require jquery_ujs
-
 //= require_tree .
 
-
-//= re
 
 //= require js/now-ui-kit.js
 //= require js/core/bootstrap.min.js
