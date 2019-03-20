2ffdd67a5a0c99fa22c0320daefabc1c774bcd50
jiaad <abdulziad08@outlook.com>
2019-03-20 17:32:54 +0100

    revert 28981b886c1c6f44b780abf4576dbaca728184d1

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -1,9 +1,19 @@
 FactoryBot.define do
   factory :user do
+<<<<<<< HEAD
+    first_name { Faker::Name.first_name }
+    last_name { Faker::Name.last_name }
+    email { Faker::Internet.email }
+    zip_code { 75000 }
+    password { 123456 }
+    town {"paris"}
+    country {"France"}
+=======
     first_name { "MyString" }
     last_name { "MyString" }
     address { "MyString" }
     zip_code { 1 }
     town { "MyString" }
+>>>>>>> 0f44e1701731aacc5664ed6b2f5cf7ab857f44aa
   end
 end
