9a62830a7dccc8591e9cc853008e8ca8504d880c
slim2244 <seb_93@live.fr>
2019-03-21 13:43:25 +0100

    fix databases

--- db/migrate/20190311142751_create_products.rb
+++ db/migrate/20190311142751_create_products.rb
@@ -1,7 +1,7 @@
 class CreateProducts < ActiveRecord::Migration[5.2]
   def change
     create_table :products do |t|
-      t.string :title
+      t.text :title
       t.decimal :price
       t.text :description
 
