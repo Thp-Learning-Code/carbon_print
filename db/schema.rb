d79549b5719e9ac20b26e01e95ff0156480856c3
TheMindset <mulezig2014@outlook.fr>
2019-03-22 15:34:45 +0100

    migration

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_03_21_082742) do
+ActiveRecord::Schema.define(version: 2019_03_21_171811) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -47,11 +47,6 @@ ActiveRecord::Schema.define(version: 2019_03_21_082742) do
     t.datetime "updated_at", null: false
     t.bigint "type_id"
     t.bigint "brand_id"
-<<<<<<< HEAD
-    t.decimal "result"
-=======
-
->>>>>>> development
     t.bigint "warehouse_id"
     t.index ["brand_id"], name: "index_products_on_brand_id"
     t.index ["type_id"], name: "index_products_on_type_id"
@@ -88,9 +83,9 @@ ActiveRecord::Schema.define(version: 2019_03_21_082742) do
     t.float "latitude"
     t.float "longitude"
     t.string "country"
-    t.boolean "is_admin", default: false
     t.string "provider"
     t.string "uid"
+    t.boolean "is_admin", default: false
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
