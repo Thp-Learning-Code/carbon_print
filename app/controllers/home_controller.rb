a1924a17151af36f736ce0985ea03b1e7db6f806
jiaad <abdulziad08@outlook.com>
2019-03-22 16:03:42 +0100

    nokogiri sommented out

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -3,9 +3,9 @@ class HomeController < ApplicationController
   require 'open-uri'
   def index
     @product = Product.all
-    page = Nokogiri::HTML(open("https://maconscienceecolo.com/knowledge-base/100-gestes-ecolos/"))
-    n = rand(0..99)
-    @tips = page.xpath('//*[@id="kb-article-content"]/ol/li')[n].text
+    # page = Nokogiri::HTML(open("https://maconscienceecolo.com/knowledge-base/100-gestes-ecolos/"))
+    # n = rand(0..99)
+    # @tips = page.xpath('//*[@id="kb-article-content"]/ol/li')[n].text
     if user_signed_in? && current_user.town != nil
       url = "https://api.airvisual.com/v2/nearest_city?lat=#{current_user.latitude}&lon=#{current_user.longitude}&key=BbFES4LurEehNo6PR"
       url_two ='https://api.airvisual.com/v2/city?city=Paris&state=Ile-de-France&country=France&key=BbFES4LurEehNo6PR'
