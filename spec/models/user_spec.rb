ff2574a566e87fa28a30e54f96c3d08da2755326
bamss <amadougolfa98@gmail.com>
2019-03-22 14:00:00 +0100

    modif user_spec.rb

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,34 +1,43 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
-  it "shouldn't create a user " do 
-    User.create(first_name: 'bob', zip_code: 75000, town: 'Butte chaumont')
-    expect(User.count).to eq(0)
-  end
-  it "shouldn't create a user " do 
-    User.create(first_name: 'bob', last_name: 'lenon', town: 'Butte chaumont')
-    expect(User.count).to eq(0)
-  end
-  it "shouldn't create a user " do 
-    User.create(last_name: 'lenon', zip_code: 75000, town: 'Butte chaumont')
-    expect(User.count).to eq(0)
-  end
-<<<<<<< HEAD
+before(:each) do
+  @user = User.create(first_name: "Bimss", last_name: "Golf", email: "bimss@gmail.com", password: "1234563", address: "28 bolivar", zip_code: 75019, town: "Paris")
+end
 
-  context "association" do
-    describe "user" do
-      it {should have_many(:products).through(:footprints)}
-    end
-    describe "user" do
-      it {should have_many(:footprints)}
-    end
+it "has a valid User" do
+  expect(@user).to be_a(User)
+end
+
+context "validation" do
+
+  it "is valid with valid attributes" do
+    expect(@user).to be_a(User)
   end
 
+  # describe "#first_name" do
+  #   it { expect(@user).to validate_presence_of(:first_name) }
+  # end
+
+  # describe "#last_name" do
+  #   it { expect(@user).to validate_presence_of(:last_name) }
+  # end
+
+  # describe "#zip_code" do
+  #   it { expect(@user).to validate_length_of(:zip_code), minimum: 5 }
+  # end
+
+  # describe "#password" do
+  #   it { expect(@user).to validate_length_of(:password), minimum: 6 }
+  # end
 end
-=======
-  it "should create a user" do
-    User.create(first_name: 'bob', last_name: 'lenon', zip_code: 75000, town: 'Butte chaumont')
-    expect(User.count).to eq(1)
-  end
+
+  # context "association" do
+  #   describe "user" do
+  #     it {should have_many(:products).through(:footprints)}
+  #   end
+  #   describe "user" do
+  #     it {should have_many(:footprints)}
+  #   end
+  # end
 end
->>>>>>> development
